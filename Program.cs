b097a4fbdc4010f0831fb0fd606aaf4a9f2a36fe
kestutis kestutis <kestutismarius12@gmail.com>
2024-10-07 21:07:59 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System.ComponentModel;
+using System.Drawing;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("   /\\ ");
+Console.WriteLine("  /  \\ ");
+Console.WriteLine(" /    \\ ");
+Console.WriteLine("/______\\ ");
+
+Console.WriteLine("    *****");
+Console.WriteLine("  **     **");
+Console.WriteLine(" **       **");
+Console.WriteLine("** Helloo  **");
+Console.WriteLine("**  From   **");
+Console.WriteLine(" **  c    **");
+Console.WriteLine("  **     ** ");
+Console.WriteLine("    *****");
+
+Console.WriteLine("    /  \\");
+Console.WriteLine("   /    \\");
+Console.WriteLine("  /      \\");
+Console.WriteLine(" /        \\");
+
+
+
+
+int  age = 38;
+Console.WriteLine("Man yra:"+ age + " metai ");
+int sum = 10 - 5;
+Console.WriteLine (sum);
+int anotherSum = age - sum;
+Console.WriteLine (anotherSum);
+string name = "Kestutis";
+
+
+
+Console.WriteLine("Preke: oboulys");
+Console.WriteLine("kiekis: 5");
+Console.WriteLine("Kaina: 2eur");
+
+
+
+Console.WriteLine("============VIZITINE=============");
+Console.WriteLine("Vardas: Kestutis   Medzius  ");
+Console.WriteLine("Amzius:38  ");
+Console.WriteLine("Pareigos: software Developer ");
+Console.WriteLine("E pastas:  kestutismarius12@gmail.com");
+Console.WriteLine("Tel.: +37064460351");
+Console.WriteLine("============VIZITINE=============");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
